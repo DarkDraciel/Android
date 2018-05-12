b50c0d1939959d724adc2305c66008f8c5ef9284
DarkDraciel <Originofsimetry32@hotmail.com>
2018-05-11 19:52:13 -0500

    Android
    Programs

---
+++ MyApplication/app/src/main/java/com/example/josluis/myapplication/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.josluis.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        login=(Button)this.findViewById(R.id.button);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Log.class);
+                startActivity(i);
+
+            }
+        });
+    }
+
+
+}
